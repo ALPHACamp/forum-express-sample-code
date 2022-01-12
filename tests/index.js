74a0d4def82fd4ee9dbc3450b2f3d2cd08f832ba
Carrot7712 <carrot7712@gmail.com>
2022-01-12 21:27:53 +0800

    feat:Assignment R04 Like/Unlike - pass test

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,6 @@ const request = require('supertest')
 
 const app = require('../app')
 
-<<<<<<< HEAD
 describe('# 測試環境初始化', function () {
   context('# First Test Case', () => {
     it(' GET /admin/users ', (done) => {
@@ -12,19 +11,5 @@ describe('# 測試環境初始化', function () {
           done()
         })
     })
-=======
-describe('# 測試環境初始化', function() {
-    
-  context('# First Test Case', () => {
-
-    it(" GET /admin/users ", (done) => {
-        request(app)
-          .get('/')
-          .end(function(err, res) {
-            done()
-        });
-    });
-
->>>>>>> upstream/R04-test
   })
 })
