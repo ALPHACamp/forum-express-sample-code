45da777a39d7f4923a448b6c98e4532cee04ebb2
Kaleb Kuo <klkuo.cx@gmail.com>
2022-08-06 10:17:36 +0800

    refactor: api error handler

--- services/admin-services.js
+++ services/admin-services.js
@@ -33,8 +33,11 @@ const adminServices = {
   deleteRestaurant: (req, cb) => {
     Restaurant.findByPk(req.params.id)
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
+        if (!restaurant) {
+          const err = new Error("Restaurant didn't exist!")
+          err.status = 404
+          throw err
+        }
         return restaurant.destroy()
       })
       .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
